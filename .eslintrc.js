0a70d16723bb4872d692d9b1eb228ade60d5e1ca
Paula Rolim <rolim.paula1999@gmail.com>
2022-01-27 21:19:07 -0300

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'eslint-plugin-import-helpers'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        '@typescript-eslint/explicit-function-return-type': ['error'],
+      },
+    },
+  ],
+  rules: {
+    'arrow-body-style': ['error', 'as-needed'],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['/^react/', 'module', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': ['error', { ignoreRestArgs: false }],
+  },
+};
