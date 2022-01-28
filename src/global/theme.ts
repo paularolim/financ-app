b4fddb1275b19ccb51e27ddf6dac2086d7c2a41b
Paula Rolim <rolim.paula1999@gmail.com>
2022-01-28 20:07:32 -0300

    chore: improve style config

---
+++ src/global/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    primary: '#FF897E',
+    primary_light: '#FF897E80',
+    secondary: '#314BCE',
+    secondary_light: '#314BCE80',
+    text: '#151940',
+    text_light: '#7F8192',
+  },
+};
