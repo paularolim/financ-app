baafffe985d1b6ecac0118257ed1c9be0e183c47
Paula Rolim <rolim.paula1999@gmail.com>
2022-03-17 22:20:22 -0300

    feat: add Icon component

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import VectorIcon from 'react-native-vector-icons/Ionicons';
+
+import { theme } from '../../global/theme';
+import { IconProps } from './types';
+
+/**
+ * Icon
+ * @example
+ * <Icon
+    name={icon}
+    size="default"
+    color="white"
+  />
+ */
+export const Icon = ({
+  name,
+  color = 'white',
+  size = 'default',
+  margin = 0,
+  marginRight = 0,
+  marginLeft = 0,
+  marginHorizontal = 0,
+  marginVertical = 0,
+}: IconProps): JSX.Element => {
+  const styles = StyleSheet.create({
+    icon: {
+      margin,
+      marginRight,
+      marginLeft,
+      marginHorizontal,
+      marginVertical,
+    },
+  });
+
+  return (
+    <VectorIcon
+      name={name}
+      size={theme.iconSize[size]}
+      color={color}
+      style={styles.icon}
+    />
+  );
+};
---
+++ src/components/Icon/types.ts
@@ -0,0 +1,14 @@
+import Icon from 'react-native-vector-icons/dist/glyphmaps/Ionicons.json';
+
+import { theme } from '../../global/theme';
+
+export interface IconProps {
+  name: keyof typeof Icon;
+  color?: keyof typeof theme.colors;
+  size?: keyof typeof theme.iconSize;
+  margin?: number;
+  marginRight?: number;
+  marginLeft?: number;
+  marginHorizontal?: number;
+  marginVertical?: number;
+}
