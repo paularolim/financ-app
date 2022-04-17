9d24391a859dbbe2781c279c24082176679e7b7a
Paula Rolim <rolim.paula1999@gmail.com>
2022-04-17 18:35:30 -0300

    feat: reset date and type after add a transaction

--- src/modais/TransactionForm/index.tsx
+++ src/modais/TransactionForm/index.tsx
@@ -73,6 +73,8 @@ export const TransactionForm = ({
       date,
       () => {
         setRegisterSuccess(true);
+        setDate(new Date());
+        setTransactionType(null);
         reset();
       },
       error => {
